42c7e443910fdfd5ba5960feb25612a202fa5104
Your Name <you@example.com>
2023-07-17 16:59:49 +0800

    edit  memory.cpp as lvim for Android

--- memory.cpp
+++ memory.cpp
@@ -1,22 +1,29 @@
+#include <cstdio>
+#include <cwchar>
 #include <iostream>
 #include <ostream>
 using namespace std;
 
 auto mauemorytest(void) -> void {
   bool great = false;
+  FILE* fp = fopen("ow.bin", "wb");
   unsigned long* p = new unsigned long[8];
   for (int i; i < 8; i++) {
+
     cout << p[i] << endl;
     if (great == false) {
+      fwrite(p[i], sizeof(long), 1,fp);
       p[i] = 0x66ccff;
       cout << p[i] <<endl;
       great = true;
     }
-      if (great)  {
-      delete [] p; 
-    }
+      
   }
-
+  if (great) {
+    fclose(fp);
+    delete [] p;
+    p = nullptr;
+  }
   /*
    * create a new memory as linux
    * kernel virtual memory
