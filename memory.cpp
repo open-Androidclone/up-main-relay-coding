1b369e7598f97e9b636dce3f6cc75d92923ecaae
et2300 <81605900+et2300@users.noreply.github.com>
2023-07-18 18:26:11 +0800

    Update memory.cpp

--- memory.cpp
+++ memory.cpp
@@ -1,37 +1,27 @@
 #include <cstdio>
-#include <cwchar>
 #include <iostream>
-#include <ostream>
-using namespace std;
 
-auto mauemorytest(void) -> void {
+auto memoryTest() -> void {
   bool great = false;
   FILE* fp = fopen("ow.bin", "wb");
   unsigned long* p = new unsigned long[8];
-  for (int i; i < 8; i++) {
-
-    cout << p[i] << endl;
-    if (great == false) {
-      fwrite(p[i], sizeof(long), 1,fp);
+  for (int i = 0; i < 8; i++) {
+    std::cout << p[i] << std::endl;
+    if (!great) {
+      fwrite(&p[i], sizeof(unsigned long), 1, fp);
       p[i] = 0x66ccff;
-      cout << p[i] <<endl;
+      std::cout << p[i] << std::endl;
       great = true;
     }
-      
   }
   if (great) {
     fclose(fp);
-    delete [] p;
+    delete[] p;
     p = nullptr;
   }
-  /*
-   * create a new memory as linux
-   * kernel virtual memory
-   * next up main is kde-yyds
-   */
 }
-//kde-yyds 
-int main(int argc, char *argv[]) {
-  mauemorytest();
+
+int main(int argc, char* argv[]) {
+  memoryTest();
   return 0;
 }
