17e69c164a5c16202dd63adf366334be2d84029d
Your Name <you@example.com>
2023-12-04 11:45:42 +0800

    fix bug memory of zero in memory test function

--- memory.cpp
+++ memory.cpp
@@ -1,27 +1,31 @@
 #include <cstdio>
 #include <iostream>
 
-auto memoryTest() -> void {
+auto memoryTest() -> unsigned long {
   bool great = false;
   FILE* fp = fopen("ow.bin", "wb");
   unsigned long* p = new unsigned long[8];
   for (int i = 0; i < 8; i++) {
     std::cout << p[i] << std::endl;
-    if (!great) {
-      fwrite(&p[i], sizeof(unsigned long), 1, fp);
-      p[i] = 0x66ccff;
-      std::cout << p[i] << std::endl;
-      great = true;
+    if (!great && i <= 9){ 
+        fwrite(&p[i], sizeof(unsigned long), 1, fp);
+        p[i] = 0x66ccff;
+        std::cout << p[i] << std::endl;
     }
   }
+  great = true;
   if (great) {
-    fclose(fp);
-    delete[] p;
-    p = nullptr;
+      fclose(fp);
   }
+  return std::move(p[0]);      
 }
 
+
 int main(int argc, char* argv[]) {
-  memoryTest();
+    auto ret = memoryTest();
+    if(ret){
+        // todo 
+            
+    }
   return 0;
 }
