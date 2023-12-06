63a2a0f408ae43eb19331227395f17aef4093e06
NAME <mail@gmail.com>
2023-12-06 15:04:28 +0800

    add a thread open

--- memory.cpp
+++ memory.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <iostream>
+#include <thread>
 
 auto memoryTest() -> unsigned long {
   bool great = false;
@@ -7,7 +8,7 @@ auto memoryTest() -> unsigned long {
   unsigned long* p = new unsigned long[8];
   for (int i = 0; i < 8; i++) {
     std::cout << p[i] << std::endl;
-    if (!great && i <= 9){ 
+    if (!great && i > 0 && i <= 8){
         fwrite(&p[i], sizeof(unsigned long), 1, fp);
         p[i] = 0x66ccff;
         std::cout << p[i] << std::endl;
@@ -17,15 +18,17 @@ auto memoryTest() -> unsigned long {
   if (great) {
       fclose(fp);
   }
-  return std::move(p[0]);      
+  return std::move(p[0]);
 }
 
 
 int main(int argc, char* argv[]) {
+    std::thread t{};
+    
     auto ret = memoryTest();
-    if(ret){
-        // todo 
-            
+    if(ret == 0x66ccff){
+        t.join();
     }
   return 0;
 }
+
